28ffe1a09f48c7b4d541818a2af9c942c687862d
arethare <aretha1906@gmail.com>
2023-11-26 23:37:40 +0700

    tugas portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["I'm a student at SMKN 1 Karawang, majoring in software engineering."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
